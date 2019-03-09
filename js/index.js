b49de137ebf69ba64c8a7a78589a366e72567fe5
Hui-LingPhillips <huilinglou@gmail.com>
2019-03-09 15:30:05 +0000

    Dropdowns
    Dropdowns started working
    
    added more JS but not working yet

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-$('.dropdown-toggle').dropdown()
+// web page is rendered in the browser before any
+//jQuery code executes.
+$(document).ready(() => {
+
+//navbar dropdowns
+
+  $('#CafeDropdown').on('click', () => {
+    $('#cafe-dd-menu').show();
+    })
+    
+  $('#TeahouseDropdown').on('click', () => {
+    $('#teahouse-dd-menu').show();
+  })
+
+
+
+  $('#carouselIndicators').carousel({
+    interval: 2000
+  })
+
+  $('.accordian').collapse({
+    toggle: false
+  })
+
+  $().button('toggle')
+
+})
